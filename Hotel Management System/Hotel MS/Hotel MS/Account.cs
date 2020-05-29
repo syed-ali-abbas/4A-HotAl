39066fd50c4131290c8d905451c8ecf8716dd630
M ARBAB ANJUM <arbabanjum0347@gmail.com>
2020-05-29 04:21:12 -0700

    Login form Updated

--- Hotel Management System/Hotel MS/Hotel MS/Account.cs
+++ Hotel Management System/Hotel MS/Hotel MS/Account.cs
@@ -31,23 +31,10 @@ namespace Hotel_MS
 
         private void button1_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
+
             Home home = new Home();
             this.Hide();
             home.Show();
-=======
-            String query = "Select username, Password from Customer_Information where username='"+textBox1.Text+"' and Password = '"+textBox2.Text+"'";
-            if (databaseHelper.loginForm(query)==1)
-            {
-                
-                MessageBox.Show("Done");                // Put here the next form after login
-            }
-            else
-            {
-                MessageBox.Show("Attention needed your input is incorect");
-            }
-
->>>>>>> 6af7d467222ff0423e005f0e223c0666552a29a3
         }
         private void button2_Click(object sender, EventArgs e)
         {
